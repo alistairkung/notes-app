15867c177ccf5c5d57b20fd092f020d43c7d1fd8
Alistair Kung <alistaircckung@gmail.com>
2017-07-17 15:10:47 +0100

    isTruthy,Falsey assertions

--- spec/assert.js
+++ spec/assert.js
@@ -7,4 +7,24 @@ var assert = {
       return false;
     }
   }
+  isTruthy: function(a) {
+    if (a){
+      console.log("Test passed: " + a + " is truthy")
+      return true
+    }
+    else {
+      return false
+      throw new Error(a + "is not true")
+    }
+  }
+  isFalsey: function(a) {
+    if (!a){
+      console.log("Test passed: " + a + " is falsey")
+      return true
+    }
+    else {
+      return false
+      throw new Error(a + "is true")
+    }
+  }
 };
