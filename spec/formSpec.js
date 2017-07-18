a63c4303a3b73e70ce36c909e1db2aa4f34a2b01
tbscanlon <thomas.scanlon@outlook.com>
2017-07-18 15:39:19 +0100

    Create function for making form and textarea

---
+++ spec/formSpec.js
@@ -0,0 +1,13 @@
+createForm();
+
+// form is of the correct type
+assert.isEqual(document.getElementById("notes-form").localName, "form")
+
+// textarea is of the correct type
+assert.isEqual(document.getElementById("notes-input").localName, "textarea")
+
+// form has the correct ID
+assert.isEqual(document.getElementById("notes-form").id, "notes-form");
+
+// textarea has the correct ID
+assert.isEqual(document.getElementById("notes-input").id, "notes-input");
