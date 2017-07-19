160fe4a5a3cc34276fdc731337a476d93af55f9d
tbscanlon <thomas.scanlon@outlook.com>
2017-07-19 14:52:08 +0100

    the BIG REFACTOR

--- spec/noteDisplaySpec.js
+++ spec/noteDisplaySpec.js
@@ -1,14 +1,19 @@
+// Displaying Notes
+// ----------------
 list = new List();
 note1 = new Note("note1");
 note2 = new Note("note2");
+
+// Add notes to list
 list.addNote(note1);
 list.addNote(note2);
-var listGen = function() {
-  var list = document.createElement("ul")
-  list.id = "ls"
-  document.body.appendChild(list)
-};
-listGen();
-list.notes.forEach(noteGen);
-var retrievedNote = document.getElementsByTagName("li")[1].innerHTML
-assert.isEqual(retrievedNote, "note2")
+
+// Create a unordered list element
+htmlGen.createList();
+
+// Add notes to unordered list
+list.notes.forEach(htmlGen.createListItem);
+
+var retrievedNote = document.getElementsByTagName("li")[1].innerHTML;
+
+assert.isEqual(retrievedNote, "note2");
