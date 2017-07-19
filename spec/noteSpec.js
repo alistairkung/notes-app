88def911d3476a42a63fd4b8c1c6233a7fe228dd
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-19 16:14:46 +0100

    Adding functionality that truncates notes over 20 char - and related spec

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -3,3 +3,6 @@ assert.isEqual(note.getContent(), "");
 
 note = new Note("content");
 assert.isEqual(note.getContent(), "content")
+
+var noteTwo = new Note("This is a very, very, very long string");
+assert.isEqual(noteTwo.truncate(), "This is a very, ver...")
